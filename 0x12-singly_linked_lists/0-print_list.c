bca1c2b19633dd0ed5ba167bf141afb7a4b8090f
perisoseko56 <perisoseko56@gmail.com>
2023-09-21 16:31:20 -0700

    printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,24 +4,22 @@
 #include "lists.h"
 /**
  * print_list -  prints all the elements of a list_t list
- * @list_t: a list
  * @h:a node in the list
  * Return:Always 0.
  */
 size_t print_list(const list_t *h)
 
 {
-	if (head == NULL)
-		printf("[0] (nil)\n");
-
 	const list_t *current = h;
-	
 	size_t count = 0;
+
+	if (h == NULL)
+		printf("[0] (nil)\n");
 	while (current != NULL)
 	{
-	printf("%d -> ", current->data);
-	current = current->next;
+	printf("%s -> ", current->str);
 
+	current = current->next;
 	count++;
 	}
 
