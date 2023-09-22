b04d6fa0f43f7da6e4358dd8a4c3044357bd55ec
perisoseko56 <perisoseko56@gmail.com>
2023-09-21 22:24:42 -0700

    deleting cause of deferral

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers.
+ * @a:integer to be reversed
+ * @n: nunber of integers.
+ * Return: reversed integer
+ */
+void reverse_array(int *a, int n)
+{
+	int *a;
+	int n;
+
+
--- 0x0B-malloc_free/README.md
+++
@@ -1,1 +0,0 @@
-malloc allocation
