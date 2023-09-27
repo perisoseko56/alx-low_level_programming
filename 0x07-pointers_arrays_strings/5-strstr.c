a8ed7e3f433aad8feacbca064ade599f155c51fd
perisoseko56 <perisoseko56@gmail.com>
2023-09-26 18:27:03 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strstr-locates a substring
+ * @haystack:string
+ * @needle:substring
+ * Return: Always o
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
