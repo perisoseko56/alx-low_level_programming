63566b0c18628b2102216f3bdf75d869c91cd824
perisoseko56 <perisoseko56@gmail.com>
2023-10-11 15:08:02 -0700

    functions pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+int op_mod(int a, int b);
+/**
+ * op_add - returns the sum of a and b
+ * @a:first integer
+ * @b:second integer
+ * Return:Always 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+	}
+/**
+ * op_sub - returns the difference of a and b
+ * @a:an integer
+ * @b: another integer
+ * Return:Always 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the product of a and b.
+ * @a:an integer
+ * @b:an integer
+ * Return: Always 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the result of the division of a by b
+ * @a:an integer
+ * @b:an mteger
+ * Return: Always 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - return remainder after dividing a and b
+ * @a:integer a
+ * @b:integer b
+ * Return:Always 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
